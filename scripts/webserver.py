8d06672d2ca6905be6cac187f6ced21c1c05e755
Leonardo Alvarez de la Campa Ovies <126499441+Decaxzm@users.noreply.github.com>
2024-10-22 21:50:42 -0500

    Update webserver.py

--- scripts/webserver.py
+++ scripts/webserver.py
@@ -1,6 +1,6 @@
-# Sumar dos números
-num1 = 1
-num2 = 1
+# Solicitar al usuario que ingrese dos números
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
 
 # Calcular la suma
 suma = num1 + num2
